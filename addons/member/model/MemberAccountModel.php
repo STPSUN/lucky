0fc100f0e7a418fffca9080b57a31697c8377240
STPSUN <497432519@qq.com>
2019-01-11 14:49:57 +0800

    1

--- addons/member/model/MemberAccountModel.php
+++ addons/member/model/MemberAccountModel.php
@@ -31,28 +31,28 @@ class MemberAccountModel extends \web\common\model\BaseModel
 //        $RewardRecordM = new \addons\fomo\model\RewardRecord();
 //        $sql = "select a.*,ifnull(sum(b.amount),0) reward_total from ({$sql}) a left join {$RewardRecordM->getTableName()} b on a.id = b.user_id group by a.id";
 //
-//        $gameM = new \addons\fomo\model\Game();
-//        $game = $gameM->getRunGame();
-//        if(!$game){
-//            $game = !empty($gameM->getLastEndGame()) ? $gameM->getLastEndGame() : [['id' => 0]];
-//        }
-//        $game = $game[0];
-//        $game_id = $game['id'];
-//
-//        $keyRecordM = new \addons\fomo\model\KeyRecord();
-//        $sql = "select a.*,ifnull(sum(key_num),0) now_key_num from ({$sql}) a left join {$keyRecordM->getTableName()} b on a.id = b.user_id and b.game_id = {$game_id} group by a.id";
-//
-//        $sql = "select a.*,ifnull(sum(key_num),0) key_total from ({$sql}) a left join {$keyRecordM->getTableName()} b on a.id = b.user_id group by a.id";
-//
-//        $sql = "select a.*,ifnull(count(game_id),0) game_count from ({$sql}) a left join {$keyRecordM->getTableName()} b on a.id = b.user_id group by a.id";
-//
-//        $tokenRecordM = new \addons\fomo\model\TokenRecord();
-//        $sql = "select a.*,ifnull(token,0) token_total from ({$sql}) a left join {$tokenRecordM->getTableName()} b on a.id = b.user_id";
+        $gameM = new \addons\fomo\model\Game();
+        $game = $gameM->getRunGame();
+        if(!$game){
+            $game = !empty($gameM->getLastEndGame()) ? $gameM->getLastEndGame() : [['id' => 0]];
+        }
+        $game = $game[0];
+        $game_id = $game['id'];
+
+        $keyRecordM = new \addons\fomo\model\KeyRecord();
+        $sql = "select a.*,ifnull(sum(key_num),0) now_key_num from ({$sql}) a left join {$keyRecordM->getTableName()} b on a.id = b.user_id and b.game_id = {$game_id} group by a.id";
+
+        $sql = "select a.*,ifnull(sum(key_num),0) key_total from ({$sql}) a left join {$keyRecordM->getTableName()} b on a.id = b.user_id group by a.id";
+
+        $sql = "select a.*,ifnull(count(game_id),0) game_count from ({$sql}) a left join {$keyRecordM->getTableName()} b on a.id = b.user_id group by a.id";
+
+        $tokenRecordM = new \addons\fomo\model\TokenRecord();
+        $sql = "select a.*,ifnull(token,0) token_total from ({$sql}) a left join {$tokenRecordM->getTableName()} b on a.id = b.user_id";
 
 
-//        $sql = "select a.*,ifnull(count(b.id),0) recommend1,GROUP_CONCAT(b.id) ids  from ({$sql}) a left join {$this->getTableName()} b on a.id = b.pid group by a.id";
+        $sql = "select a.*,ifnull(count(b.id),0) recommend1,GROUP_CONCAT(b.id) ids  from ({$sql}) a left join {$this->getTableName()} b on a.id = b.pid group by a.id";
 
-//        $sql = "select a.*,ifnull(count(b.id),0) recommend2  from ({$sql}) a left join {$this->getTableName()} b on b.pid in(a.ids) group by a.id";
+        $sql = "select a.*,ifnull(count(b.id),0) recommend2  from ({$sql}) a left join {$this->getTableName()} b on b.pid in(a.ids) group by a.id";
         return $this->getDataListBySQL($sql, $pageIndex, $pageSize, $order);
     }
     
